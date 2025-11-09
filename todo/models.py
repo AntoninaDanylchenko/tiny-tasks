f019aefc512a4fb4dd1880d356adb688deabcc43
Antonina Danylchenko <antonina.ivanets@gmail.com>
2025-11-09 09:32:25 +0100

    Create Task and Tag models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='tasks')
+    content = models.TextField()
+    deadline = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name='tasks')
+
+    def __str__(self):
+        return f"{self.content} ({self.created_at})"
