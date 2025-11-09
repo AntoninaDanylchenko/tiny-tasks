9809759daac26a2dd28f6d52ef700e91c39e44fb
Antonina Danylchenko <antonina.ivanets@gmail.com>
2025-11-09 13:30:59 +0100

    Change task-list template

--- todo/views.py
+++ todo/views.py
@@ -6,7 +6,12 @@ from todo.models import Task
 
 class TaskListView(LoginRequiredMixin, ListView):
     model = Task
+    context_object_name = 'tasks'
+    paginate_by = 10
 
     def get_queryset(self):
-        queryset = super().get_queryset()
-        return queryset.filter(user=self.request.user)
+        return (super().get_queryset()
+                .filter(user=self.request.user)
+                .select_related('user')
+                .prefetch_related('tags')
+                .order_by('is_completed', 'deadline', '-created_at'))
